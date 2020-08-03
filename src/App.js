8332e6b59a1b74f40d11e21e5995d02aeab47009
oguzbolme <bolmeoguz@gmail.com>
2020-08-04 00:52:43 +0300

    spotify widgetleri eklendi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Buyur bilader</p>
-      </header>
+    <div>
+      <h1 style={{textAlign:"center"}}>Spotifydan Seçmece Playlistler</h1>
+      <h1 style={{textAlign:"center"}}>
+        <iframe style={{margin:"10px",borderRadius:"10px"}} src="https://open.spotify.com/embed/playlist/7gIfmB7g2PrijKlgO08oZ4" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe style={{margin:"10px",borderRadius:"10px"}} src="https://open.spotify.com/embed/playlist/73wmpmwoNg4iqN9LDdeU3t" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe style={{margin:"10px",borderRadius:"10px"}} src="https://open.spotify.com/embed/playlist/5Bxyjl5wGqqBKFCyats1Ss" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe style={{margin:"10px",borderRadius:"10px"}} src="https://open.spotify.com/embed/playlist/7qPaEAghnlkuAktJhOs8wQ" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </h1>
     </div>
   );
 }
